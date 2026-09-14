12ef73e2f8288cbb421b4f9a9a943fd86b741483
Igor Budnitski <igor.budnitski@gmail.com>
2026-09-14 21:55:37 +0300

    Delete All

--- src/videos/routers/videos.router.ts
+++ src/videos/routers/videos.router.ts
@@ -2,15 +2,21 @@ import {Request, Response, Router} from "express";
 import {db} from "../../db/in-memory.db";
 import {HttpStatus} from "../../core/types/http-statuses";
 import {createErrorMessages} from "../../core/utils/error.utils";
-import { Video} from "../types/video";
-import { VideoInputDto} from "../dto/video.input.dto";
+import {Video} from "../types/video";
+import {VideoInputDto} from "../dto/video.input.dto";
 import {validateVideoInputDto} from "../validation/video-input-dto.validation";
 
 
 export const videosRouter = Router({});
 
 videosRouter
+
+    .delete('', (req: Request, res: Response) => {
+        db.videos = [];
+        res.status(HttpStatus.NoContent);
+    })
     // Get All Videos
-.get('',(req: Request, res)=>{
-    res.status(HttpStatus.Success).send(db.videos);
-})
+    .get('', (req: Request, res) => {
+        res.status(HttpStatus.Success).send(db.videos);
+    })
+
