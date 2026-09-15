68a83f5b5fdbb0ddb790a714088b54a336a6a34f
Igor Budnitski <igor.budnitski@gmail.com>
2026-09-15 20:22:34 +0300

    Fix Post

--- src/videos/routers/videos.router.ts
+++ src/videos/routers/videos.router.ts
@@ -39,7 +39,7 @@ videosRouter
             const errors = validateVideoInputDto(req.body);
 
             if (errors.length > 0) {
-                res.status(HttpStatus.BadRequest).send({errorsMessages: [{message: 'Some mistake in body', field: 'req.body'}]);
+                res.status(HttpStatus.BadRequest).send({errorsMessages: [{message: 'Incorrect body', field: 'req.body'}]});
             }
             const newVideo: Video = {
                 id: db.videos.length + 1,
