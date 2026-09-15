e360fc12560a0eb14179ad729d5292c01ee6d562
Igor Budnitski <igor.budnitski@gmail.com>
2026-09-15 19:58:16 +0300

    Fix Delete

--- src/videos/routers/videos.router.ts
+++ src/videos/routers/videos.router.ts
@@ -11,7 +11,7 @@ export const videosRouter = Router({});
 
 videosRouter
     //delete all data from DB
-    .delete('/videos/all-data', (req: Request, res: Response) => {
+    .delete('/all-data', (req: Request, res: Response) => {
         db.videos = [];
         res.send(HttpStatus.NoContent);
     })
